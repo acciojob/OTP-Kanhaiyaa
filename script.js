88fcd12b424df6333b58f4a957dc6b863477b1b8
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2024-01-02 07:24:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+    function focusNextInput(currentInput) {
+        const nextInput = currentInput.nextElementSibling;
+        if (nextInput && currentInput.value.length === 1) {
+        nextInput.focus();
+        }
+    }
+
+    function focusPrevInput(currentInput) {
+        const prevInput = currentInput.previousElementSibling;
+        if (prevInput && currentInput.value.length === 0 && event.key === 'Backspace') {
+        prevInput.focus();
+        }
+    }
